8b54d86fe154953fd8785a971309fc8365099e9f
Ruth Teshome <rteshome@charlotte.edu>
2026-02-27 16:11:39 -0500

    Implemented part1 function.

--- lab5-starter/js/part1.js
+++ lab5-starter/js/part1.js
@@ -6,16 +6,30 @@
  * @returns {string} - message 
  */
 function countCards(cards) {
-   
-        
+   let count = 0;
+
+   for (let i=0; i < cards.length; i++) {
+    if (cards[i] === 10 || cards[i] === 'J' || cards[i] === 'Q' || cards[i] === 'K' || cards[i] === 'A') {
+        count--;
+    }
+    else if ([2, 3, 4, 5, 6].includes(cards[i])) {
+        count++;
+    }
+   }
+
+   if (count > 0) {
+    return `${count} Bet`;
+   }
+
+    return `${count} Hold`;        
 }
 //uncomment following test code after implementing the function
-// console.log(countCards([2, 3, 7, 'K', 'A']));
-// console.log(countCards([2, 3, 4, 5, 6]));
-// console.log(countCards([7, 8, 9]));
-// console.log(countCards([10, 'J', 'Q', 'K', 'A']));
-// console.log(countCards([3, 7, 'Q', 8, 'A']));
-// console.log(countCards([2, 2, 10]));
-// console.log(countCards([2, 9, 'J', 2, 7]));
-// console.log(countCards([3, 2, 'A', 10, 'K']));
+ console.log(countCards([2, 3, 7, 'K', 'A']));
+ console.log(countCards([2, 3, 4, 5, 6]));
+ console.log(countCards([7, 8, 9]));
+ console.log(countCards([10, 'J', 'Q', 'K', 'A']));
+ console.log(countCards([3, 7, 'Q', 8, 'A']));
+ console.log(countCards([2, 2, 10]));
+ console.log(countCards([2, 9, 'J', 2, 7]));
+ console.log(countCards([3, 2, 'A', 10, 'K']));
 
